d2ef14a90a9d3ab4fbc395606161b37a299af752
ViniciusAlberkovics <alberkovicsvinicius@gmail.com>
2024-07-01 12:37:49 -0300

    feat: add team viewer

---
+++ install/optional/app-teamviewer.sh
@@ -0,0 +1,5 @@
+cd /tmp
+wget -O teamviewer_amd64.deb https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
+sudo apt install -y ./teamviewer_amd64.deb
+rm teamviewer_amd64.deb
+cd -
--- install/select-optional-apps.sh
+++ install/select-optional-apps.sh
@@ -1,5 +1,5 @@
 # Install optional apps
-apps=$(gum choose "1password" "DBeaver" "Discord" "Dropbox" "Obsidian" "Postman" "qBitTorrent" "Steam" "TimeShift" "VirtualBox" "WPSOffice" --no-limit --selected "Dropbox" --height 7 --header "Select optional apps")
+apps=$(gum choose "1password" "DBeaver" "Discord" "Dropbox" "Obsidian" "Postman" "qBitTorrent" "Steam" "TimeShift" "TeamViewer" "VirtualBox" "WPSOffice" --no-limit --selected "Dropbox" --height 7 --header "Select optional apps")
 
 for app in $apps; do
 	source "$OMAKUB_PATH/install/optional/app-${app,,}.sh"
