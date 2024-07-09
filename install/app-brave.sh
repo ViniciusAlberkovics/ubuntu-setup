355bcfbdca2f94f6902ffcb463a8bec4bc6d9162
ViniciusAlberkovics <alberkovicsvinicius@gmail.com>
2024-07-09 00:08:09 -0300

    add brave settings

--- install/app-brave.sh
+++ install/app-brave.sh
@@ -1,1 +1,7 @@
 flatpak install com.brave.Browser -y
+
+# register brave alias
+echo "alias brave='flatpak run com.brave.Browser'" >>~/.local/share/omakub/defaults/bash/aliases
+
+# register brave as default browser
+xdg-settings set default-web-browser com.brave.Browser.desktop
