e2cc43f2bde78b32b6c543e248da42659bb9063f
ViniciusAlberkovics <alberkovicsvinicius@gmail.com>
2024-07-10 09:11:34 -0300

    map ngrok on optional apps

--- install/select-optional-apps.sh
+++ install/select-optional-apps.sh
@@ -1,5 +1,5 @@
 # Install optional apps
-apps=$(gum choose "1password" "DBeaver" "Discord" "Dropbox" "Obsidian" "Postman" "qBitTorrent" "Steam" "TimeShift" "TeamViewer" "VirtualBox" "WPSOffice" "IriunWebCam" --no-limit --selected "Dropbox" --height 7 --header "Select optional apps")
+apps=$(gum choose "1password" "DBeaver" "Discord" "Dropbox" "Obsidian" "Postman" "qBitTorrent" "Steam" "TimeShift" "TeamViewer" "VirtualBox" "WPSOffice" "IriunWebCam" "Ngrok" --no-limit --selected "Dropbox" --height 7 --header "Select optional apps")
 
 for app in $apps; do
 	source "$OMAKUB_PATH/install/optional/app-${app,,}.sh"
