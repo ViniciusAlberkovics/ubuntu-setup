82ec6be43a3ad8efa8dcd548d8fd8210caabd49e
ViniciusAlberkovics <alberkovicsvinicius@gmail.com>
2024-11-14 19:45:40 -0300

    feat(app-obs-studio.sh): add script to install obs studio

---
+++ install/app-obs-studio.sh
@@ -0,0 +1,3 @@
+sudo apt install v4l-utils -y
+sudo apt-get install ffmpeg -y
+flatpak install flathub com.obsproject.Studio -y
---
+++ uninstall/app-obs-studio.sh
@@ -0,0 +1,2 @@
+sudo apt remove -y v4l-utils ffmpeg
+flatpak uninstall com.obsproject.Studio
