97d8445403651deaf50c80b4358dbd5eac46b8fc
ViniciusAlberkovics <alberkovicsvinicius@gmail.com>
2025-04-21 19:00:33 -0300

    change branch on download.sh

--- download.sh
+++ download.sh
@@ -3,6 +3,6 @@ sudo apt-get update > /dev/null
 sudo apt-get install -y git > /dev/null
 
 echo "Cloning master Ubuntu Setup..."
-git clone -b master https://github.com/ViniciusAlberkovics/ubuntu-setup.git ~/.local/share/omakub > /dev/null
+git clone -b do-not-use-alacritty https://github.com/ViniciusAlberkovics/ubuntu-setup.git ~/.local/share/omakub > /dev/null
 
 source ~/.local/share/omakub/install.sh
